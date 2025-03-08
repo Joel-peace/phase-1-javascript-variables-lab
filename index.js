122582405070b722ad6dc239934c333ee978bc19
Joel <joel.thayu@student.moringaschool.com>
2025-03-08 11:18:52 +0300

    this is my work

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+// 1. Declare companyName as a constant and set it to "Scuber"
+const companyName = "Scuber";
+
+// 2. Declare mostProfitableNeighborhood as a variable (so it can be reassigned)
+let mostProfitableNeighborhood = "Chelsea";
+
+// 3. Declare companyCeo as a variable (so it can be reassigned)
+let companyCeo = "Susan Smith";
+
